05c45f5e4a1c2128869aa05e9abb5255d7d0f0ba
Abhishek Khasre <abhishekkhasare@gmail.com>
2025-03-15 19:21:04 +0530

    data transformation

--- src/logger.py
+++ src/logger.py
@@ -10,20 +10,24 @@ os.makedirs(logs_dir, exist_ok=True)  # Ensure only the directory is created
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
-# Configure logging to log both to file and console
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
-)
+# Create logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)  # Set logging level
 
-# Add console handler to print logs to the console
+# Create FileHandler
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+# Create StreamHandler (for console output)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-logging.getLogger().addHandler(console_handler)
 
+# Add handlers to logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 # Testing
-# if __name__ == "__main__":
-#     logging.info("Logging has started")
+if __name__ == "__main__":
+    logger.info("Logging has started")
